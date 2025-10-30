69dff938298a28f34837bfd54180101c93fbf56c
momoxhere <satyamchaudhari789@gmail.com>
2025-10-30 21:09:59 +0530

    momo-version

--- clock.py
+++ clock.py
@@ -8,7 +8,7 @@ from time import strftime
 
 # creating tkinter window
 root = Tk()
-root.title('Clock_Junaid')
+root.title('Clock_momo')
 
 # This function is used to
 # display time on the label
@@ -20,8 +20,8 @@ def time():
 # Styling the label widget so that clock
 # will look more attractive
 lbl = Label(root, font = ('arial', 90, 'bold'),
-			background = 'white',
-			foreground = 'black')
+			background = 'yellow',
+			foreground = 'pink')
 
 # Placing clock at the centre
 # of the tkinter window
